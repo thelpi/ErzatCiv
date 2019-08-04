a01e71575ba15d0a4734c7273a3fb655039635f1
Lpi <thelpi.v3@gmail.com>
2019-08-04 13:04:04 +0200

    Tests untaires pour la gestion de la réflexion sur les types héritants de "BuildablePivot".

---
+++ ErsatzCivLibUnitTests/BuildableImplementationTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using ErsatzCivLib.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ErsatzCivLibTests
+{
+    [TestClass]
+    public class BuildableImplementationTests
+    {
+        private const string DefaultFieldName = "Default";
+        private const string CreateMethodName = "CreateAtLocation";
+
+        [TestMethod]
+        public void Check_Default_FieldOnBuildableConcreteClasses()
+        {
+            var types = GetBuildableConcreteTypes();
+            foreach (var type in types)
+            {
+                var field = type.GetField(DefaultFieldName, BindingFlags.NonPublic | BindingFlags.Static);
+                Assert.IsNotNull(field);
+                Assert.IsTrue(field.IsAssembly);
+                Assert.IsTrue(field.IsInitOnly);
+                Assert.AreEqual(type, field.FieldType);
+            }
+        }
+
+        [TestMethod]
+        public void Check_CreateAtLocation_MethodOnBuildableConcreteClasses()
+        {
+            var types = GetBuildableConcreteTypes();
+            foreach (var type in types)
+            {
+                var method = type.GetMethod(CreateMethodName, BindingFlags.NonPublic | BindingFlags.Static, null, new[] { typeof(MapSquarePivot) }, null);
+                Assert.IsNotNull(method);
+                Assert.IsTrue(method.IsAssembly);
+                Assert.AreEqual(type, method.ReturnType);
+            }
+        }
+
+        private List<Type> GetBuildableConcreteTypes()
+        {
+            return Assembly
+                .GetAssembly(typeof(BuildablePivot))
+                .GetTypes()
+                .Where(t => t.IsSubclassOf(typeof(BuildablePivot)) && !t.IsAbstract)
+                .ToList();
+        }
+    }
+}
---
+++ ErsatzCivLibUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ErsatzCivLibUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ErsatzCivLibUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1eb3601d-9789-43fa-84f0-3bda6c8481c9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
