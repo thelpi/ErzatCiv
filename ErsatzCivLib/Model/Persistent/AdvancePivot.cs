78fa8631747910a87c9d4ad9f93a3f2175237539
Lpi <thelpi.v3@gmail.com>
2019-08-05 10:31:20 +0200

    Génération de toutes les avancées scientifiques.

--- ErsatzCivLib/Model/Persistent/AdvancePivot.cs
+++ ErsatzCivLib/Model/Persistent/AdvancePivot.cs
@@ -251,6 +251,212 @@ namespace ErsatzCivLib.Model.Persistent
             Name = "Gunpowder",
             _prerequisite = new List<AdvancePivot> { Invention, IronWorking }
         };
+        public static readonly AdvancePivot Physics = new AdvancePivot
+        {
+            Era = EraPivot.MiddleAge,
+            Name = "Physics",
+            _prerequisite = new List<AdvancePivot> { Mathematics, Navigation }
+        };
+        public static readonly AdvancePivot Magnetism = new AdvancePivot
+        {
+            Era = EraPivot.MiddleAge,
+            Name = "Magnetism",
+            _prerequisite = new List<AdvancePivot> { Navigation, Physics }
+        };
+        public static readonly AdvancePivot Metallurgy = new AdvancePivot
+        {
+            Era = EraPivot.MiddleAge,
+            Name = "Metallurgy",
+            _prerequisite = new List<AdvancePivot> { Gunpowder, University }
+        };
+        public static readonly AdvancePivot Chemistry = new AdvancePivot
+        {
+            Era = EraPivot.MiddleAge,
+            Name = "Chemistry",
+            _prerequisite = new List<AdvancePivot> { University, Medicine }
+        };
+
+        public static readonly AdvancePivot AtomicTheory = new AdvancePivot
+        {
+            Era = EraPivot.ModernAge,
+            Name = "Atomic Theory",
+            _prerequisite = new List<AdvancePivot> { TheoryOfGravity, Physics }
+        };
+        public static readonly AdvancePivot Electricity = new AdvancePivot
+        {
+            Era = EraPivot.ModernAge,
+            Name = "Electricity",
+            _prerequisite = new List<AdvancePivot> { Magnetism, Metallurgy }
+        };
+        public static readonly AdvancePivot AdvancedFlight = new AdvancePivot
+        {
+            Era = EraPivot.ModernAge,
+            Name = "Advanced Flight",
+            _prerequisite = new List<AdvancePivot> { Flight, Electricity }
+        };
+        public static readonly AdvancePivot Flight = new AdvancePivot
+        {
+            Era = EraPivot.ModernAge,
+            Name = "Flight",
+            _prerequisite = new List<AdvancePivot> { Physics, Combustion }
+        };
+        public static readonly AdvancePivot Combustion = new AdvancePivot
+        {
+            Era = EraPivot.ModernAge,
+            Name = "Combustion",
+            _prerequisite = new List<AdvancePivot> { Refining, Explosives }
+        };
+        public static readonly AdvancePivot Refining = new AdvancePivot
+        {
+            Era = EraPivot.ModernAge,
+            Name = "Refining",
+            _prerequisite = new List<AdvancePivot> { Chemistry, Corporation }
+        };
+        public static readonly AdvancePivot Explosives = new AdvancePivot
+        {
+            Era = EraPivot.ModernAge,
+            Name = "Explosives",
+            _prerequisite = new List<AdvancePivot> { Gunpowder, Chemistry }
+        };
+        public static readonly AdvancePivot Corporation = new AdvancePivot
+        {
+            Era = EraPivot.ModernAge,
+            Name = "Corporation",
+            _prerequisite = new List<AdvancePivot> { Banking, Industrialization }
+        };
+        public static readonly AdvancePivot Industrialization = new AdvancePivot
+        {
+            Era = EraPivot.ModernAge,
+            Name = "Industrialization",
+            _prerequisite = new List<AdvancePivot> { Banking, Railroad }
+        };
+        public static readonly AdvancePivot Railroad = new AdvancePivot
+        {
+            Era = EraPivot.ModernAge,
+            Name = "Railroad",
+            _prerequisite = new List<AdvancePivot> { SteamEngine, BridgeBuilding }
+        };
+        public static readonly AdvancePivot SteamEngine = new AdvancePivot
+        {
+            Era = EraPivot.ModernAge,
+            Name = "Steam Engine",
+            _prerequisite = new List<AdvancePivot> { Physics, Invention }
+        };
+        public static readonly AdvancePivot Superconductor = new AdvancePivot
+        {
+            Era = EraPivot.ModernAge,
+            Name = "Superconductor",
+            _prerequisite = new List<AdvancePivot> { MassProduction, Plastics }
+        };
+        public static readonly AdvancePivot MassProduction = new AdvancePivot
+        {
+            Era = EraPivot.ModernAge,
+            Name = "MassProduction",
+            _prerequisite = new List<AdvancePivot> { Automobile, Corporation }
+        };
+        public static readonly AdvancePivot Plastics = new AdvancePivot
+        {
+            Era = EraPivot.ModernAge,
+            Name = "Plastics",
+            _prerequisite = new List<AdvancePivot> { Refining, SpaceFlight }
+        };
+        public static readonly AdvancePivot SpaceFlight = new AdvancePivot
+        {
+            Era = EraPivot.ModernAge,
+            Name = "SpaceFlight",
+            _prerequisite = new List<AdvancePivot> { Rocketry, Computers }
+        };
+        public static readonly AdvancePivot Rocketry = new AdvancePivot
+        {
+            Era = EraPivot.ModernAge,
+            Name = "Rocketry",
+            _prerequisite = new List<AdvancePivot> { AdvancedFlight, Electronics }
+        };
+        public static readonly AdvancePivot Computers = new AdvancePivot
+        {
+            Era = EraPivot.ModernAge,
+            Name = "Computers",
+            _prerequisite = new List<AdvancePivot> { Electronics, Mathematics }
+        };
+        public static readonly AdvancePivot Electronics = new AdvancePivot
+        {
+            Era = EraPivot.ModernAge,
+            Name = "Electronics",
+            _prerequisite = new List<AdvancePivot> { Electricity, Engineering }
+        };
+        public static readonly AdvancePivot Automobile = new AdvancePivot
+        {
+            Era = EraPivot.ModernAge,
+            Name = "Automobile",
+            _prerequisite = new List<AdvancePivot> { Combustion, Steel }
+        };
+        public static readonly AdvancePivot Steel = new AdvancePivot
+        {
+            Era = EraPivot.ModernAge,
+            Name = "Steel",
+            _prerequisite = new List<AdvancePivot> { Industrialization, Metallurgy }
+        };
+        public static readonly AdvancePivot Robotics = new AdvancePivot
+        {
+            Era = EraPivot.ModernAge,
+            Name = "Robotics",
+            _prerequisite = new List<AdvancePivot> { Plastics, Computers }
+        };
+        public static readonly AdvancePivot Recycling = new AdvancePivot
+        {
+            Era = EraPivot.ModernAge,
+            Name = "Recycling",
+            _prerequisite = new List<AdvancePivot> { Democracy, MassProduction }
+        };
+        public static readonly AdvancePivot NuclearFission = new AdvancePivot
+        {
+            Era = EraPivot.ModernAge,
+            Name = "Nuclear Fission",
+            _prerequisite = new List<AdvancePivot> { MassProduction, AtomicTheory }
+        };
+        public static readonly AdvancePivot NuclearPower = new AdvancePivot
+        {
+            Era = EraPivot.ModernAge,
+            Name = "Nuclear Power",
+            _prerequisite = new List<AdvancePivot> { NuclearFission, Electronics }
+        };
+        public static readonly AdvancePivot LaborUnion = new AdvancePivot
+        {
+            Era = EraPivot.ModernAge,
+            Name = "Labor Union",
+            _prerequisite = new List<AdvancePivot> { MassProduction, Communism }
+        };
+        public static readonly AdvancePivot Communism = new AdvancePivot
+        {
+            Era = EraPivot.ModernAge,
+            Name = "Communism",
+            _prerequisite = new List<AdvancePivot> { Philosophy, Industrialization }
+        };
+        public static readonly AdvancePivot Conscription = new AdvancePivot
+        {
+            Era = EraPivot.ModernAge,
+            Name = "Conscription",
+            _prerequisite = new List<AdvancePivot> { Republic, Explosives }
+        };
+        public static readonly AdvancePivot FusionPower = new AdvancePivot
+        {
+            Era = EraPivot.ModernAge,
+            Name = "Fusion Power",
+            _prerequisite = new List<AdvancePivot> { NuclearPower, Superconductor }
+        };
+        public static readonly AdvancePivot GeneticEngineering = new AdvancePivot
+        {
+            Era = EraPivot.ModernAge,
+            Name = "Genetic Engineering",
+            _prerequisite = new List<AdvancePivot> { Medicine, Corporation }
+        };
+
+        public static readonly AdvancePivot FutureTech = new AdvancePivot
+        {
+            Era = EraPivot.ModernAge,
+            Name = "Future Tech",
+            _prerequisite = new List<AdvancePivot> { FusionPower }
+        };
 
         #endregion
     }
