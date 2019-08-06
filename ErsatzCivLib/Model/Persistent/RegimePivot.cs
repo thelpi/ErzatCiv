63e6c6bbc579782cebf8a1c09283d7c48f80259b
Lpi <thelpi.v3@gmail.com>
2019-08-06 13:57:03 +0200

    Gestion des régimes politiques.

--- ErsatzCivLib/Model/Persistent/RegimePivot.cs
+++ ErsatzCivLib/Model/Persistent/RegimePivot.cs
@@ -1,47 +1,203 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 
 namespace ErsatzCivLib.Model.Persistent
 {
+    /// <summary>
+    /// Represents a political regime.
+    /// </summary>
+    /// <seealso cref="IEquatable{T}"/>
     [Serializable]
     public class RegimePivot : IEquatable<RegimePivot>
     {
+        /// <summary>
+        /// Name.
+        /// </summary>
         public string Name { get; private set; }
+        /// <summary>
+        /// Corruption rate.
+        /// </summary>
+        public double CorruptionRate { get; private set; }
+        /// <summary>
+        /// Gold cost, as each turn, for miltary units.
+        /// </summary>
+        public int UnitCost { get; private set; }
+        /// <summary>
+        /// Commerce bonus, on each <see cref="MapSquarePivot"/> of the city area, including the city itself.
+        /// </summary>
+        public int CommerceBonus { get; private set; }
+        /// <summary>
+        /// Worker's efficiency rate.
+        /// </summary>
+        public double WorkerEffiencyRate { get; private set; }
+        /// <summary>
+        /// Impact on mood for each military unit in garrison in a city.
+        /// </summary>
+        /// <example>A <c>0.5</c> value means that 2 units make 1 <see cref="CityPivot.CitizenPivot"/> from unhappy to content.</example>
+        public double GarrisonMoodRate { get; private set; }
+        /// <summary>
+        /// Science discovery speed rate.
+        /// </summary>
+        public double ScienceRate { get; private set; }
 
         private RegimePivot() { }
 
+        #region IEquatable implementation
+
+        /// <summary>
+        /// Checks if this instance is equal to another one.
+        /// </summary>
+        /// <param name="other">The other instance.</param>
+        /// <returns><c>True</c> if equals; <c>False</c> otherwise.</returns>
         public bool Equals(RegimePivot other)
         {
             return Name == other?.Name;
         }
 
-        public static bool operator ==(RegimePivot ms1, RegimePivot ms2)
+        /// <summary>
+        /// Operator "==" override. Checks equality between two instances.
+        /// </summary>
+        /// <param name="r1">The first <see cref="CivilizationPivot"/>.</param>
+        /// <param name="r2">The second <see cref="CivilizationPivot"/>.</param>
+        /// <returns><c>True</c> if equals; <c>False</c> otherwise.</returns>
+        public static bool operator ==(RegimePivot r1, RegimePivot r2)
         {
-            if (ms1 is null)
+            if (r1 is null)
             {
-                return ms2 is null;
+                return r2 is null;
             }
 
-            return ms1.Equals(ms2) == true;
+            return r1.Equals(r2) == true;
         }
 
-        public static bool operator !=(RegimePivot ms1, RegimePivot ms2)
+        /// <summary>
+        /// Operator "!=" override. Checks non-equality between two instances.
+        /// </summary>
+        /// <param name="r1">The first <see cref="CivilizationPivot"/>.</param>
+        /// <param name="r2">The second <see cref="CivilizationPivot"/>.</param>
+        /// <returns><c>False</c> if equals; <c>True</c> otherwise.</returns>
+        public static bool operator !=(RegimePivot r1, RegimePivot r2)
         {
-            return !(ms1 == ms2);
+            return !(r1 == r2);
         }
 
+        /// <inheritdoc />
         public override bool Equals(object obj)
         {
             return obj is RegimePivot && Equals(obj as RegimePivot);
         }
 
+        /// <inheritdoc />
         public override int GetHashCode()
         {
             return Name.GetHashCode();
         }
 
+        #endregion
+
         #region Static instances
 
-        public static readonly RegimePivot Despotism = new RegimePivot { Name = "Despotism" };
+        /// <summary>
+        /// Despotism regime.
+        /// </summary>
+        public static readonly RegimePivot Despotism = new RegimePivot
+        {
+            Name = "Despotism",
+            UnitCost = 0,
+            GarrisonMoodRate = 1,
+            CommerceBonus = 0,
+            CorruptionRate = 0.5,
+            WorkerEffiencyRate = 0.5,
+            ScienceRate = 0.5
+        };
+        /// <summary>
+        /// Monarchy regime.
+        /// </summary>
+        public static readonly RegimePivot Monarchy = new RegimePivot
+        {
+            Name = "Monarchy",
+            UnitCost = 0,
+            GarrisonMoodRate = 0.5,
+            CommerceBonus = 0,
+            CorruptionRate = 0.75,
+            WorkerEffiencyRate = 1,
+            ScienceRate = 0.75
+        };
+        /// <summary>
+        /// Republic regime.
+        /// </summary>
+        public static readonly RegimePivot Republic = new RegimePivot
+        {
+            Name = "Republic",
+            UnitCost = 1,
+            GarrisonMoodRate = 1 / (double)3,
+            CommerceBonus = 0,
+            CorruptionRate = 0.75,
+            WorkerEffiencyRate = 1,
+            ScienceRate = 1
+        };
+        /// <summary>
+        /// Anarchy regime.
+        /// </summary>
+        public static readonly RegimePivot Anarchy = new RegimePivot
+        {
+            Name = "Anarchy",
+            UnitCost = 0,
+            GarrisonMoodRate = 0,
+            CommerceBonus = 0,
+            CorruptionRate = 0,
+            WorkerEffiencyRate = 0.5,
+            ScienceRate = 0
+        };
+        /// <summary>
+        /// Democraty regime.
+        /// </summary>
+        public static readonly RegimePivot Democraty = new RegimePivot
+        {
+            Name = "Democraty",
+            UnitCost = 1,
+            GarrisonMoodRate = 1 / (double)5,
+            CommerceBonus = 1,
+            CorruptionRate = 0,
+            WorkerEffiencyRate = 1.5,
+            ScienceRate = 1.5
+        };
+        /// <summary>
+        /// Communism regime.
+        /// </summary>
+        public static readonly RegimePivot Communism = new RegimePivot
+        {
+            Name = "Communism",
+            UnitCost = 0,
+            GarrisonMoodRate = 0.5,
+            CommerceBonus = 0,
+            CorruptionRate = 0.75,
+            WorkerEffiencyRate = 1,
+            ScienceRate = 1
+        };
+
+        private static List<RegimePivot> _instances = null;
+
+        /// <summary>
+        /// List of every <see cref="RegimePivot"/> instances.
+        /// </summary>
+        public static IReadOnlyCollection<RegimePivot> Instances
+        {
+            get
+            {
+                if (_instances == null)
+                {
+                    _instances = typeof(RegimePivot)
+                        .GetFields(BindingFlags.Static | BindingFlags.Public)
+                        .Select(f => f.GetValue(null) as RegimePivot)
+                        .Where(v => !(v is null))
+                        .ToList();
+                }
+                return _instances;
+            }
+        }
 
         #endregion
     }
